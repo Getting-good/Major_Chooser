a1c4dbc0d8f014ec4a9975c23973fb787210f400
Jaehyeok An <aj.jae6@gmail.com>
2026-06-15 13:55:44 +0900

    AboutPage for providing info to user

--- MajorChooser_FE/src/App.jsx
+++ MajorChooser_FE/src/App.jsx
@@ -1,14 +1,25 @@
+
 import React from 'react';
 
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import MajorChooser from './pages/MajorChooserPage';
+import About from './pages/AboutPage';
+import { Link } from "react-router-dom";
 const App = () => {
   
   return (
     <div>
       <Router>
+
+          <nav>
+          <Link to="/">Home</Link>
+          {" | "}
+          <Link to="/about">About</Link>
+        </nav>
+
           <Routes>
             <Route path='/' exact element={<MajorChooser />} />
+            <Route path='/about' element={<About />} />
           </Routes>
       </Router>
       
