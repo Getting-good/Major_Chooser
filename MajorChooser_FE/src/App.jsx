d31d1895b7c648c6ae2912b5a67f94ea32ca18f2
JakeAn <aj.jae6@gmail.com>
2026-07-31 19:56:02 +0900

    UI/UX designing

--- MajorChooser_FE/src/App.jsx
+++ MajorChooser_FE/src/App.jsx
@@ -4,26 +4,23 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import MajorChooser from './pages/MajorChooserPage';
 import About from './pages/AboutPage';
+import Updates from './pages/UpdatesPage';
+
+import ManageQuestion from './pages/ManageQuestionPage';
 import { Link } from "react-router-dom";
 const App = () => {
   
   return (
     <div>
       <Router>
-
-          <nav>
-          <Link to="/">Home</Link>
-          {" | "}
-          <Link to="/about">About</Link>
-        </nav>
-
+        
           <Routes>
             <Route path='/' exact element={<MajorChooser />} />
             <Route path='/about' element={<About />} />
+            <Route path='/updates' element={<Updates />} />
+            <Route path='/manageQuestion' element={<ManageQuestion />} />
           </Routes>
       </Router>
-      
-      {/* <MajorChooser /> */}
     </div>
  
   );
